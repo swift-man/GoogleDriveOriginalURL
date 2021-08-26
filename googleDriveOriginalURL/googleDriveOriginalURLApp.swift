cea20cc1fb0d467c9f395f944f051f14f54a278b
jiniopening <jiniopening@naver.com>
2021-08-26 09:24:26 +0900

    Initial Commit

---
+++ googleDriveOriginalURL/googleDriveOriginalURLApp.swift
@@ -0,0 +1,17 @@
+//
+//  googleDriveOriginalURLApp.swift
+//  googleDriveOriginalURL
+//
+//  Created by 김승진 on 2021/08/26.
+//
+
+import SwiftUI
+
+@main
+struct googleDriveOriginalURLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
